213caac828f9358f268961847693056514477549
Fadouachemradkhi <fadwachemradkhi@gmail.com>
2025-08-31 15:33:07 +0100

    Update image imports to use absolute paths

--- resources/js/pages/Landing/Sections/AboutUs.tsx
+++ resources/js/pages/Landing/Sections/AboutUs.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ArrowRight } from 'lucide-react';  
-import vector from '../../../../../public/images/vector.png';
-import about from '../../../../../public/images/about.png';
-import Boutton from '../../../components/Boutton';
+import Boutton from '@/components/Boutton';
 
 function AboutUs() {
     return (
@@ -11,11 +9,11 @@ function AboutUs() {
                 <span className='font-semibold text-[32px] md:text-[40px] leading-[116%] tracking-normal text-gradient text-center md:text-left'>
                     À propos d'OFOQY
                 </span>
-                <img src={vector} className='mt-10 md:mt-20 w-[150px] md:w-auto mr-6' />
+                <img src='/images/vector.png' className='mt-10 md:mt-20 w-[150px] md:w-auto mr-6' />
             </div>  
             <div className='flex flex-col md:flex-row justify-between gap-6 md:gap-4 mt-6'>
                 <div className='overflow-hidden w-full md:w-[265px] h-[350px] md:h-[410px] rounded-br-[200px] md:rounded-br-[300px] mx-auto md:mx-0'>
-                    <img src={about} className='w-full h-full object-cover' />
+                    <img src='/images/about.png' className='w-full h-full object-cover' />
                 </div>
                 <div className='w-full md:w-[600px] mt-6 md:mt-10'>
                     <span className='font-normal text-[24px] md:text-[30px] leading-[32px] tracking-[0]'>
